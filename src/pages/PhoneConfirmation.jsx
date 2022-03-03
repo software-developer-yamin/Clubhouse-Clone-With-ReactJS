3e728c769e48069a6eebab269b70237f3b62b178
DeveloperYamin <developer.yamin@gmail.com>
2022-03-03 19:39:39 +0600

    Add & Style PhoneConfirmation Page

--- src/pages/PhoneConfirmation.jsx
+++ src/pages/PhoneConfirmation.jsx
@@ -3,7 +3,16 @@ import styles from "../styles/PhoneConfirmation.module.css";
 
 function PhoneConfirmation() {
   const [value, setValue] = useState("");
-  return <div className={styles.PhoneConfirmationContainer} >PhoneConfirmation</div>;
+  return (
+    <div className={styles.PhoneConfirmationContainer}>
+      <h1>Enter your phone</h1>
+      <p>
+        By entering your number, you're agreeing to our{" "}
+        <span>Terms of Service and Privacy Policy. </span>
+        Thanks!
+      </p>
+    </div>
+  );
 }
 
 export default PhoneConfirmation;
